2d0a499db82806b3effa421c189018527792a9f6
Joe Collins <joe.collins121@gmail.com>
2017-01-29 08:40:48 -0700

    Added handler and wait group, general refactors

--- server-hash.go
+++ server-hash.go
@@ -1,42 +1,62 @@
 package main
 
 import (
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"encoding/json"
 	"crypto/sha512"
 	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-  "time"
+	"time"
 )
 
-type Password struct {
-	Password string
+var lis net.Listener
+
+type Handler struct {
+	w sync.WaitGroup
+	*http.ServeMux
 }
 
-func handlePosts(w http.ResponseWriter, r *http.Request) {
-  time.Sleep(2 * time.Second)
-  postPassword(w, r)
+func main() {
+	var err error
+	lis, err = net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	h := &Handler{ServeMux: http.NewServeMux()}
+	h.ServeMux.HandleFunc("/", hashPassword)
+	h.ServeMux.HandleFunc("/shutdown", close)
+
+	fmt.Println("Listening on:", lis.Addr())
+	http.Serve(lis, h)
+
+	h.w.Wait()
 }
 
-func postPassword(w http.ResponseWriter, r *http.Request) {
-  fmt.Println(r.FormValue("password"))
-	json.NewEncoder(w).Encode(getSHA256(r.FormValue("password")))
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.w.Add(1)
+	defer h.w.Done()
+	h.ServeMux.ServeHTTP(w, r)
+	w.(http.Flusher).Flush()
 }
 
-func handleRequests() {
-	http.HandleFunc("/", handlePosts)
-	fmt.Println("Listening...")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+// '/'
+func hashPassword(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	json.NewEncoder(w).Encode(getSHA512(r.FormValue("password")))
 }
 
-func getSHA256(pw string) string {
+func getSHA512(pw string) string {
 	h := sha512.New()
 	h.Write([]byte(pw))
 	sha1_hash := base64.StdEncoding.EncodeToString(h.Sum(nil))
 	return string(sha1_hash)
 }
 
-func main() {
-	handleRequests()
+// '/shutdown'
+func close(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Finishing requests...")
+	lis.Close()
 }
